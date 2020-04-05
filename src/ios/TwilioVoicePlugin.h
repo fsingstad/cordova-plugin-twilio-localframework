c8f97c58d2f5c482debc88673fa6d9bc31a1d9d5
fsingstad <31435957+fsingstad@users.noreply.github.com>
2020-04-05 21:08:26 +0200

    add local framework

---
+++ src/ios/TwilioVoicePlugin.h
@@ -0,0 +1,14 @@
+//
+//  TwilioVoicePlugin.h
+//  TwilioVoiceExample
+//
+//  Created by Jeffrey Linwood on 3/11/17.
+//  Updated by Adam Walker 3/27/2020.
+//
+//
+
+#import <Cordova/CDV.h>
+
+@interface TwilioVoicePlugin : CDVPlugin
+
+@end
